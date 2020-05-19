11e1c6f75fd0a6b2d9521a4353f21a4cdec351ab
qiaoshuai <1145938682@qq.com>
2020-05-19 15:02:28 +0800

    更新数据库配置文件

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '128.1.137.99',
     // 数据库名
     'database'        => 'oiladmin_flv_pet',
     // 用户名
